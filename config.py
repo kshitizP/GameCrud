cf141fe8186b23b497fbd426f7c867f535554599
shubhamkumar <shubham23.kumar@paytm.com>
2024-01-19 01:56:48 +0530

    add pooling

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/game_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/game_db2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle' : 280}
